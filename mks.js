f1b1c46f224e90f16cf3ddcb8cbe153912d69122
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2023-09-10 11:21:40 +0800

    Changed to an API

--- mks.js
+++ mks.js
@@ -2,27 +2,37 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const fs = require('fs');
+const express = require('express');
+const cors = require('cors'); // Import the 'cors' package
 
-const webScraper = async () => {
-    const html = await axios.get('https://mkspamp.com.my/m/prices.xhtml');
-    const $ = await cheerio.load(html.data);
-    let data = [];
-    $('table.table-full tbody tr:nth-child(1)').each((i, elem) => {
-        var beli = $(elem).find('td.royalblue label').text();
-        var jual = $(elem).find('td.darkteal label').text();
-        var currentdate = new Date(); 
-        if (i <= 1) {
-            data.push({
-                date: currentdate,
-                buy: beli.replace(/\s/g, '') / 1000,
-                sell: jual.replace(/\s/g, '') / 1000
-            })
-        }
-    });
-    fs.writeFile('./mks.json', JSON.stringify(data), (error) => {
-        if (error) throw error;
-    })
-    console.log(data);
-}
-webScraper();
+const app = express();
+const port = 3000; // Choose a port for your API
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.get('/data', async (req, res) => {
+    try {
+        const html = await axios.get('https://mkspamp.com.my/m/prices.xhtml');
+        const $ = await cheerio.load(html.data);
+        let data = [];
+        $('table.table-full tbody tr:contains("Gold   : 1 Tael 999.9 (MYR)")').each((i, elem) => {
+            const index = $(elem).find('td.darkteal label').text();
+            if (i <= 1) {
+                data.push({
+                    index: index
+                })
+            }
+        });
+
+        // Respond with JSON data
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'An error occurred'});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`API server is running on port ${port}`);
+});
