ee6dd58ea6c212849f9f08c2e493d261c27eb3d4
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2023-12-11 13:08:13 +0800

    Modified API

--- mks.js
+++ mks.js
@@ -16,15 +16,39 @@ app.get('/data', async (req, res) => {
         const html = await axios.get('https://mkspamp.com.my/m/prices.xhtml');
         const $ = await cheerio.load(html.data);
         let data = [];
-        $('table.table-full tbody tr:contains("Gold   : 1 Tael 999.9 (MYR)")').each((i, elem) => {
-            const index = $(elem).find('td.darkteal label').text();
+
+        $('table.table-full tbody tr:contains("Gold   : 1 Kilo 999.9 (MYR)")').each((i, elem) => {
+            console.log($(elem).find('td.darkteal label').text())
             if (i <= 1) {
                 data.push({
-                    index: index
+                    oneKilo: $(elem).find('td.darkteal label').text()
                 })
             }
         });
 
+        $('table.table-full tbody tr:contains("Gold   : 100 GM 999.9 (MYR)")').each((i, elem) => {
+            if (i <= 1) {
+                data.push({
+                    oneHundredGram: $(elem).find('td.darkteal label').text()
+                })
+            }
+        });
+
+        $('table.table-full tbody tr:contains("Gold   : 50 GM 999.9 (MYR)")').each((i, elem) => {
+            if (i <= 1) {
+                data.push({
+                    fiftyGram: $(elem).find('td.darkteal label').text()
+                })
+            }
+        });
+
+        $('table.table-full tbody tr:contains("Gold   : 1 Tael 999.9 (MYR)")').each((i, elem) => {
+            if (i <= 1) {
+                data.push({
+                    oneTael: $(elem).find('td.darkteal label').text()
+                })
+            }
+        });
         // Respond with JSON data
         res.json(data);
     } catch (error) {
